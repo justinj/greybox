801e3adaf9decb0d80d5cbf41e2e9bf73586a08d
Justin Jaffray <justin.jaffray@gmail.com>
2013-10-23 15:02:49 -0400

    Some more config metaprogramming

--- lib/greybox/configurable.rb
+++ lib/greybox/configurable.rb
@@ -12,12 +12,39 @@ module Greybox
         @defaults[name] = args[:default] if args.has_key? :default
         @required_properties ||= Set.new
         @required_properties << name if args[:required]
-        define_method(setter) do |value|
-          @properties ||= {}
-          @properties[name] = value
-        end
 
-        define_method(getter) { get_prop(name) }
+        # Stolen from RubyTapas episodes 27 and 28
+        mod = if const_defined?(:Properties, false)
+                const_get(:Properties)
+              else
+                new_mod = Module.new do
+                  def self.to_s
+                    "Properties(#{properties.join(", ")})"
+                  end
+
+                  def self.properties
+                    instance_methods(false).reject { |m| m.to_s.end_with? "=" }
+                  end
+
+                  def self.inspect
+                    self.to_s
+                  end
+                end
+                const_set(:Properties, new_mod)
+              end
+
+        mod.module_eval %Q{
+          def #{setter}(value)
+            @properties ||= {}
+            @properties[:#{name}] = value
+          end
+
+          def #{getter}
+            get_prop(:#{name})
+          end
+        }
+
+        include mod
       end
 
       def method_missing(name, *args)
@@ -30,6 +57,7 @@ module Greybox
     end
 
     def verify
+      p self.class.ancestors
       self.class.required_properties.each do |prop|
         raise "Property #{prop} is required." unless get_prop(prop)
       end
