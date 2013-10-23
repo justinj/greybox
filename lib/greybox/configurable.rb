e3e945c496cd8e1135bd0f546954a75758759c24
Justin Jaffray <justin.jaffray@gmail.com>
2013-10-23 15:40:43 -0400

    extract method

--- lib/greybox/configurable.rb
+++ lib/greybox/configurable.rb
@@ -8,29 +8,14 @@ module Greybox
       def def_property(name, args = {})
         setter = "#{name}=".to_sym
         getter = name
-        @defaults ||= {}
-        @defaults[name] = args[:default] if args.has_key? :default
-        @required_properties ||= Set.new
-        @required_properties << name if args[:required]
+        (@defaults ||= {})[name] = args[:default] if args.has_key? :default
+        (@required_properties ||= Set.new) << name if args[:required]
 
         # Stolen from RubyTapas episodes 27 and 28
         mod = if const_defined?(:Properties, false)
                 const_get(:Properties)
               else
-                new_mod = Module.new do
-                  def self.to_s
-                    "Properties(#{properties.join(", ")})"
-                  end
-
-                  def self.properties
-                    instance_methods(false).reject { |m| m.to_s.end_with? "=" }
-                  end
-
-                  def self.inspect
-                    self.to_s
-                  end
-                end
-                const_set(:Properties, new_mod)
+                const_set(:Properties, new_blank_properties_module)
               end
 
         mod.module_eval %Q{
@@ -47,17 +32,29 @@ module Greybox
         include mod
       end
 
-      def method_missing(name, *args)
-        raise "Property #{name} was not defined"
+      private
+      def new_blank_properties_module
+        Module.new do
+          def self.to_s
+            "Properties(#{properties.join(", ")})"
+          end
+
+          def self.inspect
+            self.to_s
+          end
+
+          def self.properties
+            instance_methods(false).reject { |m| m.to_s.end_with? "=" }
+          end
+        end
       end
     end
 
     def get_prop(prop)
-       (@properties || {}).fetch(prop, self.class.defaults[prop]) 
+      (@properties || {}).fetch(prop, self.class.defaults[prop]) 
     end
 
     def verify
-      p self.class.ancestors
       self.class.required_properties.each do |prop|
         raise "Property #{prop} is required." unless get_prop(prop)
       end
