fd8df81da42f41bdcab075cb7bfaeddc334b0ac2
Justin Jaffray <justin.jaffray@gmail.com>
2013-10-31 14:56:23 -0400

    improve test coverage in greybox.rb, closes #2

--- spec/greybox_spec.rb
+++ spec/greybox_spec.rb
@@ -8,5 +8,31 @@ module Greybox
         end.must_raise RuntimeError
       end
     end
+
+    describe "running the tests" do
+      it "defers to runner to actually run the tests" do
+        Runner.any_instance.expects(:run)
+        Greybox.setup do |c|
+          c.input = "test/*.input"
+          c.test_command = "echo hi"
+        end
+      end
+    end
+
+    describe "printing the failures" do
+      it "prints out each failure" do
+        Diffy::Diff.stubs(:new).returns("-a\n+b\n")
+        $stdout.expects(:puts).with("FAILED:")
+        $stdout.expects(:puts).with("filename: a.input")
+        $stdout.expects(:puts).with("-a\n+b\n")
+        Runner.any_instance.stubs(:failures).returns([{filename: "a.input",
+                                                       expected: "a",
+                                                       actual: "b"}])
+        Greybox.setup do |c|
+          c.input = "test/*.input"
+          c.test_command = "echo hi"
+        end
+      end
+    end
   end
 end
